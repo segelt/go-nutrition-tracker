b5a28addc36ecaea76543d585b686c1c51ae93b2
segelt <86981324+segelt@users.noreply.github.com>
2023-03-01 07:29:21 +0300

    user registration

--- mongo/auth.go
+++ mongo/auth.go
@@ -1,8 +1,13 @@
 package mongo
 
 import (
-	"fmt"
+	"context"
+	"html"
 	"nutritiontracker/resource"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var _ resource.AuthService = (*AuthService)(nil)
@@ -16,5 +21,34 @@ func NewAuthService(db *DB) *AuthService {
 }
 
 func (s *AuthService) RegisterUser(create resource.UserInput) error {
-	return fmt.Errorf("not implemented")
+	newUser := resource.User{
+		ID:       primitive.NewObjectID(),
+		Username: create.Username,
+		Password: create.Password,
+	}
+	err := processUserInfoBeforeRegistration(&newUser)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), s.db.operationTimeout)
+	defer cancel()
+
+	coll := s.db.client.Database("nutrition-tracker").Collection("users")
+	_, err = coll.InsertOne(ctx, newUser)
+	return err
+}
+
+func processUserInfoBeforeRegistration(u *resource.User) error {
+	//turn password into hash
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+
+	//remove spaces in username
+	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
+
+	return nil
 }
